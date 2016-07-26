a6a53e6ff554b820b42954f6230c8d272e9bdff7
Joe Phearse <joephearse@Joes-MacBook-Air.local>
2016-07-26 11:09:27 -0500

    comming for first time

---
+++ public/resume.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="assets/ico/favicon.ico">
+
+    <title>Joe Phearse</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/welcome.css" rel="stylesheet">
+    <link href="/css/font.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <!-- Static navbar -->
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="welcome.php">Home</a></li>
+            <li class="active"><a href="resume.php">About</a></li>
+            <li><a href="#works">Works</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+
+	<div id="aboutwrap">
+	    <div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-lg-offset-3">
+				</div>
+			</div><! --/row -->
+	    </div> <!-- /container -->
+	</div><! --/aboutwrap -->
+	
+	<div class="container">
+		<div class="row centered mt mb">
+			<div class="col-lg-8 col-lg-offset-2">
+				<h2>Joe D. Phearse</h2>
+				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+				<h2>Education</h2><hr>
+				<p><strong>The University of Texas at San Antonio</strong></strong></p>
+				<p><em><strong>San Antonio, Texas</strong> </em></p>
+				<div id="textbox">
+					<p class="alignleft">M.A. in Education, <em>Concentration in Instructional Technology</em></p>
+					<p class="alignright">August 2015 - Present</p>
+				</div>
+				<div style="clear: both;"></div>
+				<div id="textbox">
+					<p class="alignleft">B.A. in Economics</p>
+					<p class="alignright">May 2014 - August 2015</p>
+				</div>
+				<div style="clear: both;"></div>
+				<p><strong>The University of Houston</strong></p>
+				<p><em><strong>Houston, Texas</strong> </em></p>
+				<div id="textbox">
+					<p class="alignleft">Computer Science</p>
+					<p class="alignright">August 2012 - May 2014</p>
+				</div>
+				<div style="clear: both;"></div>
+
+				<h2>Work Experience</h2><hr>
+				<div id="textbox">
+					<p class="alignleft"><strong>Lead Inventory</strong></p>
+					<p class="alignright">August 2014 - July 2016</p>
+				</div>
+				<div style="clear: both;"></div>
+				<p class="job-location"><em>Carmax, San Antonio, Texas</em></p>
+				<ul>
+					<li>Executes quality control checks on any received/expected inventory</li>
+					<li>Innovates beneficial facility and ground maintenance techniques to enhance inventory quality</li>
+					<li>Maintains computerized records of tracking inventory</li>
+					<li>Communicates with staff members storewide to better serve customers</li>
+				</ul>
+				<div id="textbox">
+					<p class="alignleft"><strong>Guest Services Representative</strong></p>
+					<p class="alignright">September 2011 - August 2012</p>
+				</div>
+				<div style="clear: both;"></div>
+				<p class="job-location"><em>Westwood Cinema 6, Brenham, Texas</em></p>
+				<ul>
+					<li>Collected/sold tickets to customers</li>
+					<li>Assisted guests with complaints and special requests</li>
+					<li>Ushered and maintained customer orderliness during peak hours</li>
+					<li>Provided comfortable atmosphere for guests through sociability and cleanliness</li>
+				</ul>
+				<div id="textbox">
+					<p class="alignleft"><strong>Power Supply Unit Tester Intern</strong></p>
+					<p class="alignright">April 2011 - August 2011</p>
+				</div>
+				<div style="clear: both;"></div>
+				<p class="job-location"><em>HDL Research Lab, Brenham, Texas</em></p>
+				<ul>
+					<li>Tested unitary components of power supply technology through electric simulation</li>
+					<li>Ensured that products met military standards</li>
+					<li>Interacted with professionals across various departments to maintain the checks and balances of the
+					company</li>
+				</ul>
+
+				<h2>Recognition</h2><hr>
+				<div id="textbox">
+					<p class="alignleft"><strong>Dean's List</strong></p>
+					<p class="alignright">Summer 2014</p>
+				</div>
+				<div style="clear: both;"></div>
+				<p class="recog"><em>University of Texas at San Antonio, San Antonio, Texas</em></p>
+				<div id="textbox">
+					<p class="alignleft"><strong>Store Champion (Employee of the Month)</strong></p>
+					<p class="alignright">April 2015, February 2016</p>
+				</div>
+				<div style="clear: both;"></div>
+				<p class="recog"><em>Carmax, San Antonio, Texas</em></p>
+
+				<h2>Skills</h2><hr>
+				<ul>
+					<li><strong>Computer:</strong> experienced with HTML, CSS, JavaScript, Apache Subversion (SVN), SCORM, and GIMP, proficient in Microsoft Office</li>
+					<li><strong>Problem Solving:</strong> experience working with deadlines and managing multiple, demanding tasks as well as creating innovative and efficient solutions</li>
+					<li><strong>Social Engagement:</strong> able to communicate verbally and in writing in a positive manner with colleagues and clientele</li>
+					<li><strong>Work Ethic:</strong> detail-oriented in both straightforward and complex tasks, enjoys challenging and engaging work, capable of taking initiative as well as following orders and working without supervision, adaptable in new environments</li>
+				</ul>
+
+			</div>
+
+
+
+
+		</div><! --/row -->
+	</div><! --/container -->
+	
+	
+
+	<div id="footerwrap">
+		<div class="container">
+			<div class="row centered">
+				<div class="col-lg-4">
+					<p><b>Codeup Student</b></p>
+				</div>
+			
+				<div class="col-lg-4">
+					<p>San Antonio.</p>
+				</div>
+				<div class="col-lg-4">
+					<p>Jphearse@gmail.com</p>
+				</div>
+			
+			</div>
+		
+		</div>
+	</div><! --/footerwrap -->
+	
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+     <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="/js/bootstrap.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+  </body>
+</html>
