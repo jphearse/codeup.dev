7b26b6e8d296d772a69d38a34854e72d661e7f72
Joe Phearse <jphearse@gmail.com>
2016-07-27 16:47:19 -0500

    added comments/ cleaned up

--- public/js/if-else.js
+++ public/js/if-else.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/* Grade average example*/
 var test1 = 70;
 var test2 = 80;
 var test3 = 95;
@@ -10,7 +10,7 @@ if ((test1 + test2 + test3)/ 3 >= 80) {
 } else  {
 	console.log("You need to practice more")
 }
-
+/*HEB example*/
 var ryan = 250;
 var cameron = 180;
 var george = 320;
@@ -21,21 +21,22 @@ var disgeorge;
 
 if (ryan >=200){
 	disryan = ryan - (ryan*dicount);
-	console.log ("Ryan spent " + ryan + " discount was applied" + " Ryan spent " + disryan);
+	console.log ("Ryan spent " + ryan + " discount was applied" + " Ryan final price " + disryan);
 }
 
 if (cameron >=200){
 	discameron = cameron - (cameron*dicount);
-	console.log ("cameron spent " + cameron + " discount was applied" + " cameron spent " + discameron);
+	console.log ("Cameron spent " + cameron + " discount was applied" + " Cameron final price " + discameron);
 } else {
-	console.log ("cameron spent " + cameron + " no discount was applied" + " cameron spent " + cameron);
+	console.log ("Cameron spent " + cameron + " no discount was applied" + " Cameron final price " + cameron);
 }
 
 if (george >=200){
 	disgeorge = george - (george*dicount);
-	console.log ("george spent " + george + " discount was applied" + " george spent " + disgeorge);
+	console.log ("George spent " + george + " discount was applied" + " George final price " + disgeorge);
 }
 
+/*Coin flip*/
 var flipACoin = Math.floor(Math.random()* 2)
 
 if (flipACoin == 0){
