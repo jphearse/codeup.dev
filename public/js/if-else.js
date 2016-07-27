1335e87ca7ec649dabce0c84f25ded821ced620a
Joe Phearse <jphearse@gmail.com>
2016-07-27 16:35:05 -0500

    initial commit

---
+++ public/js/if-else.js
@@ -0,0 +1,45 @@
+"use strict";
+
+var test1 = 70;
+var test2 = 80;
+var test3 = 95;
+var flipACoin = Math.floor(Math.random()* 2)
+
+if ((test1 + test2 + test3)/ 3 >= 80) {
+	console.log ("You're awesome!")
+} else  {
+	console.log("You need to practice more")
+}
+
+var ryan = 250;
+var cameron = 180;
+var george = 320;
+var dicount = .35;
+var disryan;
+var discameron;
+var disgeorge;
+
+if (ryan >=200){
+	disryan = ryan - (ryan*dicount);
+	console.log ("Ryan spent " + ryan + " discount was applied" + " Ryan spent " + disryan);
+}
+
+if (cameron >=200){
+	discameron = cameron - (cameron*dicount);
+	console.log ("cameron spent " + cameron + " discount was applied" + " cameron spent " + discameron);
+} else {
+	console.log ("cameron spent " + cameron + " no discount was applied" + " cameron spent " + cameron);
+}
+
+if (george >=200){
+	disgeorge = george - (george*dicount);
+	console.log ("george spent " + george + " discount was applied" + " george spent " + disgeorge);
+}
+
+var flipACoin = Math.floor(Math.random()* 2)
+
+if (flipACoin == 0){
+	console.log ("Buy a car");
+} else {
+	console.log ("Buy a house");
+}
