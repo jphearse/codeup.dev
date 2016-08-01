18cc6e4173d58128a50c7ab410ec97c2bf7922b0
Joe Phearse <jphearse@gmail.com>
2016-08-01 11:29:58 -0500

    minor edits

--- public/js/functions.js
+++ public/js/functions.js
@@ -6,10 +6,11 @@ var myNameIs = 'Joe'; // TODO: Fill in your name here.
 // Create a function called 'sayHello' that takes a parameter 'name'.
 // When called, the function should log a message that says hello from the passed in name.
 function sayHello(name) {
-    return name;
+    console.log("Hello " + name);
+
 }
 // TODO: Call the function 'sayHello' passing the variable 'myNameIs' as a parameter.
-console.log(sayHello(myNameIs));
+sayHello(myNameIs);
 // Don't modify the following line
 // It generates a random number between 1 and 100 and stores it in random
 
@@ -19,10 +20,10 @@ console.log(sayHello(myNameIs));
 // The function should use the ternary operator to log a message.
 // The log should tell the number passed in and whether it is odd or not.
 var random = Math.floor((Math.random()*100)+1);
+	console.log(random);
 function isOdd (x){
-	
 	var number = (x % 2 == 0) ? "Even" : "Odd";
-	console.log(number + " "+random);
+	console.log(number);
 
 }
 // TODO: Call the function 'isOdd' passing the variable 'random' as a parameter.
