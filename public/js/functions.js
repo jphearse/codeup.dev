68c4c2a4da99812303ea088364ba464dd9137216
Joe Phearse <jphearse@gmail.com>
2016-08-01 11:02:27 -0500

    initial commit

---
+++ public/js/functions.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var myNameIs = 'Joe'; // TODO: Fill in your name here.
+
+// TODO:
+// Create a function called 'sayHello' that takes a parameter 'name'.
+// When called, the function should log a message that says hello from the passed in name.
+function sayHello(name) {
+    return name;
+}
+// TODO: Call the function 'sayHello' passing the variable 'myNameIs' as a parameter.
+console.log(sayHello(myNameIs));
+// Don't modify the following line
+// It generates a random number between 1 and 100 and stores it in random
+
+
+// TODO:
+// Create a function called 'isOdd' that takes a number as a parameter.
+// The function should use the ternary operator to log a message.
+// The log should tell the number passed in and whether it is odd or not.
+var random = Math.floor((Math.random()*100)+1);
+function isOdd (x){
+	
+	var number = (x % 2 == 0) ? "Even" : "Odd";
+	console.log(number + " "+random);
+
+}
+// TODO: Call the function 'isOdd' passing the variable 'random' as a parameter.
+isOdd (random)
