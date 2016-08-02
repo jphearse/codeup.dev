fc86cd2ae966bb9d741a20352be7f3dbe9809ea4
Joe Phearse <jphearse@gmail.com>
2016-08-02 14:16:01 -0500

    changed names in array

--- public/js/iterating.js
+++ public/js/iterating.js
@@ -2,7 +2,7 @@
     "use strict";
     // number in array is index/key
     // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
-    var names = ["Joe", "Emma", "Chad", "Katrina"];
+    var names = ["Kayla", "Joe", "Lia", "Stephen"];
     // TODO: Create a log statement that will log the number of elements in the names array.
     console.log(names.length);
     // TODO: Create log statements that will print each of the names array elements individually.
