f6d6d802ed391d4c3966cde7fe05ef806ca1075e
Joe Phearse <jphearse@gmail.com>
2016-08-02 13:41:33 -0500

    added IIFE

--- public/js/scope.js
+++ public/js/scope.js
@@ -1,17 +1,18 @@
 "use strict";
+(function(){
 
+
+var myNameIs = 'Joe'; // TODO: Fill in your name here.
 
 // TODO:
 // Create a function called 'sayHello' that takes a parameter 'name'.
 // When called, the function should log a message that says hello from the passed in name.
-(function sayHello(name) {
-var myNameIs = 'Joe'; // TODO: Fill in your name here.
-
+function sayHello(name) {
     console.log("Hello " + name);
 
-})();
+}
 // TODO: Call the function 'sayHello' passing the variable 'myNameIs' as a parameter.
-
+sayHello(myNameIs);
 // Don't modify the following line
 // It generates a random number between 1 and 100 and stores it in random
 
@@ -20,15 +21,15 @@ var myNameIs = 'Joe'; // TODO: Fill in your name here.
 // Create a function called 'isOdd' that takes a number as a parameter.
 // The function should use the ternary operator to log a message.
 // The log should tell the number passed in and whether it is odd or not.
-(function isOdd (x){
 var random = Math.floor((Math.random()*100)+1);
 	console.log(random);
+function isOdd (x){
 	var number = (x % 2 == 0) ? "Even" : "Odd";
 	console.log(number);
 
-})();
-
+}
 // TODO: Call the function 'isOdd' passing the variable 'random' as a parameter.
-
+isOdd (random)
 
 // in javascript if no return it returns undefined
+})();
