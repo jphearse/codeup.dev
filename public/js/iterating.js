518acdbc6be9e5e6e90ffd421a805c92e10afe19
Joe Phearse <jphearse@gmail.com>
2016-08-02 15:22:46 -0500

    added for each loop

--- public/js/iterating.js
+++ public/js/iterating.js
@@ -4,9 +4,21 @@
     // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
     var names = ["Kayla", "Joe", "Lia", "Stephen"];
     // TODO: Create a log statement that will log the number of elements in the names array.
-    console.log(names.length);
+    console.log("There are " + names.length + " elements in the array");
     // TODO: Create log statements that will print each of the names array elements individually.
     for (var i = 0; i < names.length; i += 1){
     	console.log(names[i]);
     }
+
+    names.forEach( function(name, index, array){
+    	console.log("Element at index " + index + " is " + name);
+    });
+
+    	console.log(names);
+    	var namesInReverseOrder = names.reverse();
+    	console.log(namesInReverseOrder);
+    	
+
+    
+
 })();
