609a332880b8395eaf8883ede070c046d250d9fb
Joe Phearse <jphearse@gmail.com>
2016-08-05 15:23:33 -0500

    changed nav bar

--- public/welcome.php
+++ public/welcome.php
@@ -31,28 +31,29 @@
 
   <body>
 
-    <!-- Static navbar -->
-    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-
-        </div>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="welcome.php">Home</a></li>
-            <li><a href="#about">Resume</a></li>
-            <li><a href="#works">Works</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
+<div class="container-fixed">
+<div class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#headerwrap">Joe Phearse</a>
     </div>
-
+    <div class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+       
+        <li><a href="#headerwrap">Home</a></li>
+        <li><a href="#about">About Me</a></li>
+        <li><a href="#works">Portfolio</a></li>
+       
+      </ul>
+    </div><!--/.nav-collapse -->
+  </div>
+</div>
+</div>
 
 	<div id="headerwrap">
 	    <div class="container">
