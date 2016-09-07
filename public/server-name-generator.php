2bad96f9376d8e897b4bb55ae21c41d1b57e3d72
Joe Phearse <jphearse@gmail.com>
2016-09-07 15:12:48 -0500

    added bootstrap cdn

--- public/server-name-generator.php
+++ public/server-name-generator.php
@@ -16,6 +16,7 @@ function generator($element, $element2)
 <html>
 <head>
 	<title>Server Name Generator</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<style>
 		h1{
 			font-size: 50px;
