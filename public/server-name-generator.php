31b7e6e73d8a3cf790118a1a98f09b25bf223eb8
Joe Phearse <jphearse@gmail.com>
2016-09-07 14:32:28 -0500

    creates servers names with random adjectives and random nouns, added css

---
+++ public/server-name-generator.php
@@ -0,0 +1,27 @@
+<?php 
+
+$adjectives = ['Wandering', 'Hideous', 'Fancy', 'Skillful', 'Aspiring', 'Complex', 'Awesome', 'Chubby', 'Ablaze', 'Devilish'];
+$nouns = ['tree','fork','tent','hands','thunder','baby','donkey','fairy','dog','airplane'];
+
+function randomElement($array){
+	$random = mt_rand(0,9);
+	return $array[$random];
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style>
+		h1{
+			font-size: 50px;
+			font-family: copperplate;
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<h1>Server Name: <?php echo randomElement($adjectives) . "-" . randomElement($nouns); ?></h1>
+</body>
+</html>
