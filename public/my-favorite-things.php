f627b1850db3deedc62b980b6c9c00c21856a113
Joe Phearse <jphearse@gmail.com>
2016-09-07 15:12:37 -0500

    added bootstrap cdn

--- public/my-favorite-things.php
+++ public/my-favorite-things.php
@@ -7,8 +7,11 @@ $favoriteThings = ['Not doing homework', 'Concerts', 'Movies', 'Weekends', 'Netf
 <html>
 <head>
 	<title>Favorite Things</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<style>
-	
+	table{
+		font-size: 20px;
+	}
 	tr:nth-child(even) {
 		background-color: #f2f2f2
 	}
