2c92bd190f449c2663e29b064ce92563725377bd
Joe Phearse <jphearse@gmail.com>
2016-09-07 15:34:00 -0500

    added more adjectives and nouns

--- public/server-name-generator.php
+++ public/server-name-generator.php
@@ -1,10 +1,10 @@
 <?php 
 
-$adjectives = ['Wandering', 'Hideous', 'Fancy', 'Skillful', 'Aspiring', 'Complex', 'Awesome', 'Chubby', 'Ablaze', 'Devilish'];
-$nouns = ['tree','fork','tent','hands','thunder','baby','donkey','fairy','dog','airplane'];
+$adjectives = ['Wandering', 'Hideous', 'Fancy', 'Skillful', 'Aspiring', 'Complex', 'Awesome', 'Chubby', 'Ablaze', 'Devilish', 'difficult', 'sorry','anxious','huge','cute'];
+$nouns = ['tree','fork','tent','hands','thunder','baby','donkey','fairy','dog','airplane', 'soup', 'poet', 'dealer', 'pizza', 'driver'];
 
 function randomElement($array){
-	$random = mt_rand(0,9);
+	$random = mt_rand(0,count($array)-1);
 	return $array[$random];
 }
 function generator($element, $element2)
