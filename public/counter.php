39d4abe9f97ec9ab496a68b9cf7d5457f0783d90
Joe Phearse <jphearse@gmail.com>
2016-09-08 16:29:04 -0500

    added css

--- public/counter.php
+++ public/counter.php
@@ -14,6 +14,23 @@ extract(pageController());
 <head>
 	<title>Counter</title>
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<style>
+	body {
+		padding: 10px;
+	}
+	a {
+		border-radius: 5px;
+	    background-color: red;
+	    border: 0;
+	    color: white;
+	    padding: 10px 20px; 
+	}
+	a:hover {
+		color: #4a4a4a;
+	    text-decoration: none;  
+	}
+
+	</style>
 </head>
 <body>
 <h1>Counter: <?= $counter; ?> </h1>
