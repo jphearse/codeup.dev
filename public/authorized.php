f64ebf891a2fbfe8664b798232031c2b82341c29
Joe Phearse <jphearse@gmail.com>
2016-09-09 12:04:32 -0500

    initial commit

---
+++ public/authorized.php
@@ -0,0 +1,13 @@
+<?php 
+
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Authorized</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+	<h1>Authorized</h1>
+</body>
+</html>
