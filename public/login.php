61312d828e8d6f8ba41658424ea115edb0d32d9d
Joe Phearse <jphearse@gmail.com>
2016-09-09 12:24:01 -0500

    added css and page controller function

--- public/login.php
+++ public/login.php
@@ -1,18 +1,23 @@
 <?php 
-	$name = isset($_POST['name']) ? $_POST['name'] : '';
-	$password = isset($_POST['password']) ? $_POST['password'] : '';
-	$failed = '';
-	if (!empty($_POST) || !empty($_GET)) {
+function pageController(){
+		$log = [];
+		$log['name'] = isset($_POST['name']) ? $_POST['name'] : '';
+		$log['password'] = isset($_POST['password']) ? $_POST['password'] : '';
+		$log['failed'] = '';
+		if (!empty($_POST) || !empty($_GET)) {
 
-	
-	if ($name == 'guest' && $password == 'password') {
-		header("Location: /authorized.php");
-		// remember to die!
-		die;
-	} else {
-		$failed = 'Wrong username or password';
+		
+		if ($log['name'] == 'guest' && $log['password'] == 'password') {
+			header("Location: /authorized.php");
+			// remember to die!
+			die;
+		} else {
+			$log['failed'] = 'Wrong username or password';
+		}
 	}
+	return $log;
 }
+extract(pageController());
 ?>
 
 <!DOCTYPE html>
@@ -20,15 +25,20 @@
 <head>
 	<title>Login</title>
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<style>
+		body{
+			padding: 10px;
+		}
+	</style>
 </head>
 <body>
 <h1>Login</h1>
 	<form method="POST">
 	        <label>Username</label>
-	        <input type="text" name="name"><br>
+	        <input class="form-control" type="text" name="name"><br>
 	        <label>Password</label>
-	        <input type="password" name="password"><br>
-	        <input type="submit">
+	        <input class="form-control" type="password" name="password"><br>
+	        <input class="btn btn-primary" type="submit">
 	</form>
 			<h4><?= $failed; ?></h4> 
 </body>
