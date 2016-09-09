526f9c30032291e65129c943a6ecc12e0557015b
Joe Phearse <jphearse@gmail.com>
2016-09-09 12:23:46 -0500

    added css

--- public/authorized.php
+++ public/authorized.php
@@ -6,6 +6,11 @@
 <head>
 	<title>Authorized</title>
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<style>
+		body{
+			padding: 10px;
+		}
+	</style>
 </head>
 <body>
 	<h1>Authorized</h1>
