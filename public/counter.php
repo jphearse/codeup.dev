034261649d3d8ece908a6226c64c8c920c6e909c
Joe Phearse <jphearse@gmail.com>
2016-09-09 12:05:30 -0500

    styled page

--- public/counter.php
+++ public/counter.php
@@ -34,7 +34,7 @@ extract(pageController());
 </head>
 <body>
 <h1>Counter: <?= $counter; ?> </h1>
-<a href="counter.php?counter=<?= $counter +1; ?>">Up</a>
-<a href="counter.php?counter=<?= $counter -1; ?>">Down</a>
+<a class="btn btn-info" href="counter.php?counter=<?= $counter +1; ?>">Up</a>
+<a class="btn btn-info" href="counter.php?counter=<?= $counter -1; ?>">Down</a>
 </body>
 </html>
