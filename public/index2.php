dc18d5d60f1b0342828f4e33062229303898f1e6
Joe Phearse <jphearse@gmail.com>
2016-09-13 11:22:01 -0500

    added divs and css

--- public/index2.php
+++ public/index2.php
@@ -2,10 +2,22 @@
 <html>
 <head>
 	<title>Index2</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<style>
+		#header{
+			margin-bottom: 15px;
+		}
+		.footer{
+			position:absolute;
+  			bottom:0;
+    		width:100%;
+    		height:60px;
+		}
+	</style>
 </head>
 <body>
-	<?php include 'header.php';  ?>
+	<div id="header"><?php include 'header.php';  ?></div>
 	<?php include 'nav.php';  ?>
-	<?php include 'footer.php';  ?>
+	<div class="footer"><?php include 'footer.php';  ?></div>
 </body>
 </html>
