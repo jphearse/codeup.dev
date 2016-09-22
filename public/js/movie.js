bbc314e98aa4884132713d917aaf16dc492eab14
Joe Phearse <jphearse@gmail.com>
2016-09-22 15:17:59 -0500

    added class to castSpace

--- public/js/movie.js
+++ public/js/movie.js
@@ -58,12 +58,13 @@ function request(title){
 		function movieCast(cast){
 				console.log("<img src=http://image.tmdb.org/t/p/w500" + cast.cast[0].profile_path + ">");
 				var castSpace = '';			//call cast request and use that for 
-				castSpace += "<div class='castImages'>";
 				for (var i = 0; i <= 4; i++) {
-				castSpace += "<img src=http://image.tmdb.org/t/p/w500" + cast.cast[i].profile_path + ">";
-				}
+				castSpace += "<div id='castImages' class='col-lg-3 col-md-3 col-sm-3 col-xs-3'>";
+				castSpace += "<img src=http://image.tmdb.org/t/p/w500" + cast.cast[i].profile_path + "><br>";
+				castSpace += cast.cast[i].name;
 				castSpace += "</div>"
-				$("#cast").append(castSpace);
+				}
+				$("#cast").html(castSpace);
 		}
 
 
