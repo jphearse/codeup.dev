aec58ea954e70be09ae13e256fad8238fbb83881
Joe Phearse <jphearse@gmail.com>
2016-09-22 19:53:46 -0500

    fixed code

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,6 @@
 <?php // Get new instance of PDO object
-$dbc = new PDO('mysql:host=127.0.0.1;dbname=employees', 'codeup', 'codeup')
-	;
+require_once ('db_info.php');
+$dbc = new PDO('mysql:host='.DB_HOST . ';dbname='.DB_NAME, DB_USER, DB_PASS);
 
 // Tell PDO to throw exceptions on error
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
