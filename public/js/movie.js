d2cf64ddb2fac24f07a13c5e104f128a74a4d7e8
Joe Phearse <jphearse@gmail.com>
2016-09-22 20:46:24 -0500

    added if statement to control when no cast image

--- public/js/movie.js
+++ public/js/movie.js
@@ -60,9 +60,11 @@ function request(title){
 				var castSpace = '';			//call cast request and use that for 
 				castSpace += '<h3><strong>Cast</strong></h3>';
 				for (var i = 0; i <= 4; i++) {
+				if (cast.cast[i].profile_path != null) {
 				castSpace += "<div id='castImages' class='col-lg-3 col-md-3 col-sm-3 col-xs-3'>";
-				castSpace += "<img src=http://image.tmdb.org/t/p/w500" + cast.cast[i].profile_path + "><br>";
-				castSpace += "<strong>"+ cast.cast[i].name + "</strong><br> as " + cast.cast[i].character;
+					castSpace += "<img src=http://image.tmdb.org/t/p/w500" + cast.cast[i].profile_path + "><br>";
+					castSpace += "<strong>"+ cast.cast[i].name + "</strong><br> as " + cast.cast[i].character;
+				}
 				castSpace += "</div>"
 				}
 				$("#cast").html(castSpace);
