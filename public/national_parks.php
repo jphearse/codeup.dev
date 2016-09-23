f1aa956694055745907e9219ad8c8e5d30665844
Joe Phearse <jphearse@gmail.com>
2016-09-23 16:25:25 -0500

    added bootstrap/styling

--- public/national_parks.php
+++ public/national_parks.php
@@ -12,7 +12,7 @@ function pageController($dbc)
 	$offset = (isset($_GET)) ? $_GET['offset'] : 0 ;
 	$stmt = $dbc->query('SELECT * FROM national_parks');
 	return [
-		'parks' => $dbc->query('SELECT * FROM national_parks LIMIT 4 OFFSET ' . $offset)->fetchAll(PDO::FETCH_ASSOC),
+		'parks' => $dbc->query('SELECT * FROM national_parks LIMIT 4 OFFSET ' . $offset),
 		'parkCount' => $stmt->rowCount()
 	];
 };
@@ -22,17 +22,18 @@ extract(pageController($dbc));
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>National Parks</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 <body>
-<table>
+<table class="table table-striped">
 				<th><h4>Row</h4></th>
 				<th><h4>Name</h4></th>
 				<th><h4>Location</h4></th>
 				<th><h4>Date Established</h4></th>
 				<th><h4>Area In Acres</h4></th>
 
-				<?php foreach ($parks as $index => $park) { ?>
+				<?php foreach ($parks as $key => $park) { ?>
 					<tr>
 						<td> <?= $park['id'] ?> </td>
 						<td> <?= $park['NAME'] ?> </td>
@@ -43,7 +44,7 @@ extract(pageController($dbc));
 				<?php } ?>
 			</table>
 			<?php $j = 1 ;for($i = 0; $i <= $parkCount; $i+=4) { ?>
-				<a href="national_parks.php?offset=<?=$i?>"><?=($j++)?></a>
+				<a href="national_parks.php?offset=<?= $i ?>" class="btn btn-success"><?=($j++)?></a>
 			<?php } ?>
 </body>
 </html>
